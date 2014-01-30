d56a3fb5c8a85f5ef0acdc6d3ba12ae1f0f4811d
buenjy <buenjybar@gmail.com>
2014-01-29 21:13:11 -0600

    [GRUNT] script updated + date.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,18 @@ var config_path = 'config/';
 
 module.exports = function (grunt) {
 
-//    var secret = grunt.file.readJSON(config_path + 'secret.json');
-    var keyPEM = grunt.file.readJSON(config_path + '.json').path;
 
     //Project configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         secret: grunt.file.readJSON(config_path + 'secret.json'),
-        keyPEM : grunt.file.readJSON(config_path + 'ppk.json').path,
         sftp: {
             server: {
                 files: {
+                    "images/": "images/**/*",
+                    "tmp/": "tmp/**",
+                    "js/": "js/**",
+                    "css/": "css/**",
                     "./": "index.html"
                 },
                 options: {
@@ -20,10 +21,31 @@ module.exports = function (grunt) {
                     port: '<%= secret.port %>',
                     username: '<%= secret.username %>',
                     path: '/var/www/',
-                    srcBasePath : './',
-                    privateKey: grunt.file.read(keyPEM),
-                    showProgress:true
+                    srcBasePath: './',
+                    createDirectories: true,
+                    password: '',
+                    showProgress: true
                 }
+            }
+        },
+        prompt: {
+            pass: {
+                options: {
+                    questions: [
+                        {
+                            name: 'git',
+                            message: 'sftp pass for <%= secret.username %> user?',
+                            config: 'sftp.server.options.password',
+                            type: 'password',
+                            default: ''
+                        }
+                    ]
+                }
+            }
+        },
+        exec:{
+            open:{
+                cmd: "START http://<%= secret.host %>/"
             }
         }
     });
@@ -35,6 +57,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-prompt');
+    grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('grunt-ssh');
 
     //Default task
@@ -50,5 +74,7 @@ module.exports = function (grunt) {
     });
 
     //transfer to server
-    grunt.registerTask('transfer', ['sftp:server']);
+    grunt.registerTask('transfer',
+        'ask for the password, send to the server then open the browser to see if it is well displayed',
+        ['prompt:pass:questions', 'sftp:server', 'exec:open']);
 };
