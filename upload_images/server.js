fa8fbd613898cb219a2162776a492335ecf9d459
Vianney <Vianney@pc37.home>
2014-08-21 07:31:35 -0400

    server version 2

--- upload_images/server.js
+++ upload_images/server.js
@@ -1,52 +1,54 @@
 var formidable = require('formidable'),
-	http = require('http'),
-	util = require('util'),
+    http = require('http'),
+    util = require('util'),
 	fs = require('fs-extra');
-	
-http.createServer(function(req,res){
-	
-if(req.url =='/upload' && req.method.toLowerCase() == 'post'){
-	var form = new formidable.IncomingForm();
-	
-	form.parse(req, function(err, fields, files){
-		res.writeHead(200, {'content-type' : 'text/plain'});
-		res.write('received upload\n\n');
-		res.end(util.inspect({fields : fields, files : files}));	
-	});
-	
-	form.on('progress', function(bReceived, bExpected){
-		var percent = (bReceived / bExpected) * 100;
-		console.log(percent.toFixed(2));
-	});
 
-	form.on('error', function(err){
-		console.error(err);
-	});
-	
-	
-	form.on('end', function(fields, files){
-		var tmp_path = this.openedFiles[0].path;
-		var file_name = this.openedFiles[0].name;
+http.createServer(function(req, res) {
+	if (req.url == '/subscribe' && req.method.toLowerCase() == 'post') {
 		
-		var location = 'upload';
+	}
+	
+  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+    // parse a file upload
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/html'});
+	  res.end(
+		  '<meta http-equiv="refresh" content="0; url=http://localhost:8080/" />'
+	  );
+    });
+
+	form.on('end', function(fields, files){
+		var i = 0, length = this.openedFiles.length;
+		
+		for(; i < length; ++i){
+		var tmp_path = this.openedFiles[i].path;
+		var file_name = this.openedFiles[i].name;
+		
+		var location = './uploads/';
+		file_name = file_name.replace(/\s/g, '_');
 		fs.copy(tmp_path, location + file_name, function(err){
 			if(err){
-				console.err(err);
+				console.log(err);
 			}else{
-				console.log('success\n');
+				console.log('success');
 			}
 		});
+		}
 	});
-	return;
-}	
 	
-	res.writeHead(200, {'content-type' : 'text/plain'});
-	res.end(
-		'<form action="/upload" enctype="multipart/form-data" method="post">'+
-		'<input type="file" name="upload" multiple="multiple">'+
-		'<input type="submit" value="Upload" >'+
-		'</form>'
-	);
-	
-	
-	}).listen(8080);
+    return;
+  }
+
+  // show a file upload form
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+	'Veuillez selection les images a transferer '+
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="file" name="upload" multiple="multiple" accept="image/x-png image/gif image/jpeg"><br>'+
+    '<input type="submit" value="Transferer">'+
+    '</form>'
+  );
+}).listen(8080);
+console.log("server started port 8080")
